f9ca4160cfcb19248a8a49450504462a75eaf31e
anuu92004 <annupandey1797@gmail.com>
2025-06-17 09:35:52 +0530

    Django files

---
+++ DJango/dLFmALL/mallUsers/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from.import views
+
+urlpatterns = [
+    path('signup/',views.userSignup.as_view()),
+    path("getmembershipDetails/<email>/",views.userMembership.as_view())
+]
